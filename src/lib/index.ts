8855c1414f50f98ca93ddce3e79d9ab4e8b672a7
Artandrey <artandrey7777@gmail.com>
2024-12-19 21:00:13 +0200

    refactor: remove unnecessary export for builder-accessors from index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,4 +2,3 @@ export * from './parametrized-builder';
 export * from './decorators';
 export * from './types';
 export * from './to-builder-method';
-export * from './decorators/builder-accessors';
